62e9b33af468a4ffb5b350c002ff8fc869f3b3e9
Nuru <nuruzzamannuru7595@gmail.com>
2025-06-17 11:46:13 +0600

    base fix

--- recipe-app/src/main.jsx
+++ recipe-app/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import GlobalState from "./context";
 
 createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
+    <BrowserRouter basename="/recipe-app">
         <StrictMode>
             <GlobalState>
                 <App />
